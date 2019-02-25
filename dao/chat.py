a85976de90f7b7ec947fea557a3e817167b6baea
kelvin123459 <Kelvin.garcia5@upr.edu>
2019-02-25 12:12:34 -0800

    empty dao

--- dao/chat.py
+++ dao/chat.py
@@ -1,2 +1,12 @@
 class ChatDAO:
+    def getChats(self):
+        return
+    def insertChat(self):
+        return
+    def insertMember(self):
+        return
+    def getChatByID(self):
+        return
+    def delete(self):
+        return
     
