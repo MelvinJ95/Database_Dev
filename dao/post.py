ecd872f371a3e6b9ae7bbf70020fee164ca2f6d9
Carlos Ayala <carlos.ayala11@upr.edu>
2019-02-25 12:25:38 -0400

    commit

--- dao/post.py
+++ dao/post.py
@@ -3,7 +3,9 @@ class PostsDAO:
     def getAllPosts(self):
         result = []
         post1 = [1, 'Hola', '2-24-2019', 'link.png']
+        post2 = [4, 'Adios', '2-25-2019', 'link2.png']
         result.append(post1)
+        result.append(post2)
         return result
 
     def getPostById(self, pid):
