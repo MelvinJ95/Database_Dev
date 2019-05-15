fe26a420376765d47ef7b7a1e8dd28ce1dc74f50
Carlos Ayala <carlos.ayala11@upr.edu>
2019-05-15 13:03:41 -0400

    Updated Database configuration.

--- config/db_config.py
+++ config/db_config.py
@@ -1,7 +1,7 @@
 #Databse configuration information
 
 pg_config = {
-    'user' : 'appusr',
-    'passwd' : 'minix',
-    'dbname' : 'appdb'
+    'user' : 'carlos',
+    'passwd' : 'db_app',
+    'dbname' : 'db_app'
 }
